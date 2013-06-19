14c8d405f2bd78067ec32f3c3eb7dfe8b19635c1
kim jong min <kjm259011@gmail.com>
2013-06-19 16:56:42 +0900

    initial code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dasi::Application.config.secret_token = 'ff5a3a7dcd536a990c2a022f06bced8585cf33a8bc5e93f59793409455312e806c0d11c471032fd6ccafca23ba4e6d0e6a3b1af0bda549bfd712670e1cc284ba'
