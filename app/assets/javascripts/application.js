9207fdc85a10a56bb416d9589df3d56c65eeb0de
Lee Hyeon <okmlp32@gmail.com>
2013-08-09 21:40:41 +0900

    Song upload Bug and restriction about song count(TODO : JS Refactoring)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -44,8 +44,8 @@ function add_songtag() {
 }
 
 function songtag_form_add_song() {
-	var mint_title = $("input[name='add-mint-name-in-new-songtag']")[0].value;
-	var mint_youtube_link = $("input[name='add-mint-youtube-link-in-new-songtag']")[0].value;
+	var mint_title = $("div.dialog-box input[name='add-mint-name-in-new-songtag']")[0].value;
+	var mint_youtube_link = $("div.dialog-box input[name='add-mint-youtube-link-in-new-songtag']")[0].value;
 	var patt=/(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/ ]{11})/i;
 	var mint_youtube_id = patt.exec(mint_youtube_link);
 	//잘못된 id를 넣었을 경우도 Validation Check필요할 듯. Youtube id 는 11글자인걸로 알고있음
@@ -56,10 +56,10 @@ function songtag_form_add_song() {
 	} else if(mint_youtube_id == null) {
 		alert("Please type right youtube link");
 	} else {
-		$('#add-song-list-in-new-songtag').append('<li><img src="http://img.youtube.com/vi/'
+		$("div.dialog-box ul[name='add-song-list-in-new-songtag']").append('<li><img src="http://img.youtube.com/vi/'
 									 + mint_youtube_id[1] + '/0.jpg"/>'
 									 + mint_title 
-									 + '<input id="songtag_song__youtube_id" name="songtag[song][][youtube_id]" type="hidden" value="' + mint_youtube_id + '">' 
+									 + '<input id="songtag_song__youtube_id" name="songtag[song][][youtube_id]" type="hidden" value="' + mint_youtube_id[1] + '">' 
 									 + '<input id="songtag_song__title" name="songtag[song][][title]" type="hidden" value="' + mint_title + '">');
 	}
 }
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,18 +2,26 @@ class SongsController < ApplicationController
   before_filter :user_only
   
   def create
-    params[:song][:songs].each do |s|
-      song = Song.new
-      song.title = s[:title]
-      song.tag_id = params[:song][:tag_id]
-      song.youtube_id = s[:youtube_id]
-      song.uid = params[:song][:uid]
-      song.save
-    end
-    
     @songtag = Songtag.find(params[:song][:tag_id])
     
-    redirect_to @songtag
+    if @songtag.songs.count >= 15
+      #need some alert message
+      redirect_to @songtag
+    else
+      params[:song][:songs].each do |s|
+        puts s
+        song = Song.new
+        song.title = s[:title]
+        song.tag_id = params[:song][:tag_id]
+        song.youtube_id = s[:youtube_id]
+        song.uid = params[:song][:uid]
+        song.save
+      end
+      
+      redirect_to @songtag
+    end
+    
+    
   end
 
 end
